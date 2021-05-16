74f57a1dfdad498fa50171b3f542a8c47031209d
mayank <mayanks028@gmailcom>
2021-05-16 22:47:26 +0530

    first

---
+++ src/test/java/learning/UdamyMavenTutorialWithTestNG/tt.java
@@ -0,0 +1,12 @@
+package learning.UdamyMavenTutorialWithTestNG;
+
+import org.testng.annotations.Test;
+
+public class tt {
+	
+	@Test
+	public void mm(){
+		System.out.println("dfbkdsgfkj");
+	}
+
+}
